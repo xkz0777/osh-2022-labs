c7cbc6c9e2293924b2df0a785f06a87938c23f27
xkz0777 <821376043@qq.com>
2022-05-31 14:49:19 +0800

    add readme

---
+++ lab3/README.md
@@ -0,0 +1,9 @@
+# Lab3 Report
+
+## 目录结构
+
+`chat_room_server` 是完善后的双人聊天室代码
+
+## 编译运行
+
+`cargo run` 即可
--- lab3/chat_room_server/src/main.rs
+++ lab3/chat_room_server/src/main.rs
@@ -1,26 +1,24 @@
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::{env, process, str, thread};
+use std::{env, process, thread};
 
-fn handler_chat(tcp_read: &mut TcpStream, tcp_write: &mut TcpStream) {
+// source: https://riptutorial.com/rust/example/4404/a-simple-tcp-client-and-server-application--echo
+fn handler_chat(mut tcp_read: TcpStream, tcp_write: &mut TcpStream) {
     let prompt = "Message: ".as_bytes().to_vec();
+    let mut message = prompt.clone();
     let mut buffer = [0_u8; 1024];
     while let Ok(recieved) = tcp_read.read(&mut buffer) {
         if recieved == 0 {
             // 不发空消息
             continue;
         } else {
-            let buffer_string =
-                str::from_utf8(&buffer[0..recieved]).expect("Invalid UTF-8 message");
-            buffer_string.split('\n').for_each(|line| {
-                if !line.is_empty() {
-                    let mut line = line.as_bytes().to_vec();
-                    line.push(b'\n'); // 还得加上换行符
-                    let mut message = prompt.clone();
-                    message.extend(line.iter());
+            buffer[0..recieved].iter().for_each(|x| {
+                message.push(*x);
+                if *x == b'\n' {
                     tcp_write
                         .write_all(message.as_slice()) // write_all 处理大文件
                         .expect("TCP write error");
+                    message = prompt.clone();
                 }
             });
         }
@@ -29,24 +27,29 @@ fn handler_chat(tcp_read: &mut TcpStream, tcp_write: &mut TcpStream) {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
+    if args.len() > 2 {
         println!("Invalid number of arguments");
+        process::exit(1);
+    }
+    if args.len() == 1 {
+        println!("Usage: cargo run <port>");
         process::exit(1);
     }
     let port = &args[1];
     let addr = &format!("127.0.0.1:{}", port);
     // server 开始监听
     let listener = TcpListener::bind(addr).expect("Tcp bind error");
+    println!("Server listening on port {}", port);
 
     let mut tcp1 = listener.accept().expect("Tcp accept error");
     let mut tcp2 = listener.accept().expect("Tcp accept error");
 
     // 创建两个线程，一个线程处理 tcp1，一个线程处理 tcp2，创建前先 clone 防止因为 move 丢失所有权
-    let mut tcp_clone1 = tcp1.0.try_clone().expect("Clone failed");
-    let mut tcp_clone2 = tcp2.0.try_clone().expect("Clone failed");
+    let tcp_clone1 = tcp1.0.try_clone().expect("Clone failed");
+    let tcp_clone2 = tcp2.0.try_clone().expect("Clone failed");
 
-    let thread1 = thread::spawn(move || handler_chat(&mut tcp_clone1, &mut tcp2.0));
-    let thread2 = thread::spawn(move || handler_chat(&mut tcp_clone2, &mut tcp1.0));
+    let thread1 = thread::spawn(move || handler_chat(tcp_clone1, &mut tcp2.0));
+    let thread2 = thread::spawn(move || handler_chat(tcp_clone2, &mut tcp1.0));
 
     thread1.join().expect("Thread 1 join error");
     thread2.join().expect("Thread 2 join error");
